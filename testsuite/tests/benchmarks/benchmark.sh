82d076d00ad72bc6094d5c667eac2c2fe01790b8
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-02-23 11:25:24 +0530

    Produce overlayed graphs for ocaml testsuite benchmarks

--- testsuite/tests/benchmarks/benchmark.sh
+++ testsuite/tests/benchmarks/benchmark.sh
@@ -10,6 +10,7 @@ file="${ctx}${test_file}"
 
 ( test -f "${compiler_output}"  && cat "${compiler_output}" ;
   test -f "${compiler_output2}" && cat "${compiler_output2}"
-) | sed -n "/^ *0*\([0-9]\+.[0-9]\+\)s ${pattern}$/{s//\1/p;q}" \
-  | ( read elapsed ; echo "${dir}\t${file}\t${elapsed}\tsecs" ) \
-  >> "${BENCHMARK_FILE}"
+) | awk '{if (match($0,"ocamlopt.opt")) exit; print}' \
+  | grep '^\s\s[0-9]\+\.[0-9]\+s ' \
+  | awk "{sum[\$2] += \$1} END{for (i in sum) print \"${dir}\\t${file}/\" i \"\\t\" sum[i] \"\tsecs\"}" \
+        >> "${BENCHMARK_FILE}"
