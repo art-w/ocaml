c8790dde2330f862d2c2599c127a015953d65d39
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-03-09 18:17:01 +0530

    Use opam_build to build irmin

--- bench.sh
+++ bench.sh
@@ -144,19 +144,10 @@ eval $(opam env)
 
 opam_build ocamlgraph
 
-
-
 cd irmin
 git checkout 2.9.0
-opam install -y --deps-only .
-for i in $(seq 1 "$NB_RUNS"); do
-  rm -f build.log
-  dune clean
-  OCAMLPARAM=",_,timings=1" dune build --verbose --profile=release @install 2>&1 | tee -a build.log | sed 's/^{/ {/'
-  timings "irmin"
-done
 cd ..
-
+opam_build irmin
 
 cd opam
 opam install crowbar
